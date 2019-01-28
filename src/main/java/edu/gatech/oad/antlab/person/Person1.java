844108626ab738c429921b0b8f21e418e5af9e79
rpratt1020 <40003343+rpratt1020@users.noreply.github.com>
2019-01-28 17:07:34 -0500

    Update Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -31,7 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-		String answer = ""
+		String answer = "";
 		for (int i = 2; i < input.length(); i++) {
 			answer += input.charAt(i);
 		}
